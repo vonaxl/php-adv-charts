7d0f2aa6a04400e3101bd0646b989b6f7a153f3f
vonaxl <vonaxlcorso@gmail.com>
2019-12-12 13:09:51 +0100

    removed unnecessary file

--- getData2.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    header('Content-Type: application/json');   
-    include 'data2.php';
-    $res = [];
-    foreach ($graphs['fatturato_by_agent']['data'] as $agent => $agents) {
-        $res[] = $agent;
-    }
-    echo json_encode($graphs);
